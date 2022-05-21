7bcca42ce9ed0bf44124a1d47469cb6cfdeca1a6
Thomas Simpson <thomas.simpson768@gmail.com>
2022-05-21 21:47:38 +0100

    Seed db with some airports

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Airport.create(name: "Leeds Bradford", code: "LBA")
+Airport.create(name: "Edingburgh", code: "EDI")
+Airport.create(name: "Inverness", code: "INV")
+Airport.create(name: "Wick John O Groats", code:"WCK")
