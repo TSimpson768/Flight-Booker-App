98e6346671335e22975c5d425ad929fbd86ae65b
Thomas Simpson <thomas.simpson768@gmail.com>
2022-05-29 22:31:49 +0100

    Show flights on homepage

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,9 +2,16 @@ class FlightsController < ApplicationController
 
   def index
     @flight = Flight.new
+    @airports = Airport.all
+    @flights = Flight.where(flight_params)
   end
 
   def show
     
   end
+
+  def flight_params
+    params.permit(:flight).permit(:depart_id, :arrive_id, :departure_time)
+  end
 end
+ 
