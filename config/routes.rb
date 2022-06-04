d1bcd56f256f9e50df4890e0857bf3062c283cf4
Thomas Simpson <thomas.simpson768@gmail.com>
2022-06-04 16:42:42 +0100

    Add booking route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :airports
   resources :flights
+  resources :bookings
   root 'flights#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
