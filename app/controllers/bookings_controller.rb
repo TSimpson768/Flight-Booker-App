d087a2a0cc05cf1348805d54698c1e7cc0818d17
Thomas Simpson <thomas.simpson768@gmail.com>
2022-06-05 17:47:41 +0100

    complete booking flow

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,7 +6,20 @@ class BookingsController < ApplicationController
     no_passengers.to_i.times { @booking.passengers.build }
   end
 
+  def create
+    @booking = Booking.new(create_booking_params)
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new
+    end
+  end
+
   private
+
+  def create_booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:first_name, :last_name, :address1, :address2, :address3, :address4, :address5, :postcode, :date_of_birth])
+  end
 
   def flight_id_params
     { flight_id: all_params[0] }
